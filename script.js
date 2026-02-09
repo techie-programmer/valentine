f8a158d3092aca928f8ffed0c729f1b108c8b112
Abhii <165805060+techie-programmer@users.noreply.github.com>
2026-02-09 11:53:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function yes() {
+  document.getElementById("response").innerHTML =
+    "YAYYY 💕 You just made my day!";
+}
+
+function no() {
+  document.getElementById("response").innerHTML =
+    "I’ll pretend I didn’t see that 😌";
+}
