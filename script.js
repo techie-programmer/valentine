6b01d8a10f6c8c54a5a896daf65df0c56cf72486
Abhii <165805060+techie-programmer@users.noreply.github.com>
2026-02-09 12:13:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ typeWriter();
 // No button escape
 const noBtn = document.getElementById("noBtn");
 
-noBtn.addEventListener("mouseover", () => {
+function moveButton() {
+  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+}
+
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+
   const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
   const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
   noBtn.style.position = "absolute";
